a5310edf3eb7222484857bd8bb288c4adf977f98
Tonye Andre Martial <tandremartial@internal.salesforce.com>
2026-02-20 19:50:55 -0500

    Adding the Prime function

--- src/Loops.java
+++ src/Loops.java
@@ -20,17 +20,39 @@ public class Loops {
         return true;
     }
 
-    public static void  main(String[] args) {
-        System.out.println(1 + " " + (isPrime(1) ? "is a prime number" : "is NOT a prime number"));
-        System.out.println(2 + " " + (isPrime(2) ? "is a prime number" : "is NOT a prime number"));
-        System.out.println(3 + " " + (isPrime(3) ? "is a prime number" : "is NOT a prime number"));
-        System.out.println(4 + " " + (isPrime(4) ? "is a prime number" : "is NOT a prime number"));
-        System.out.println(5 + " " + (isPrime(5) ? "is a prime number" : "is NOT a prime number"));
-        System.out.println(6 + " " + (isPrime(6) ? "is a prime number" : "is NOT a prime number"));
-        System.out.println(7 + " " + (isPrime(7) ? "is a prime number" : "is NOT a prime number"));
-        System.out.println(8 + " " + (isPrime(8) ? "is a prime number" : "is NOT a prime number"));
-        System.out.println(9 + " " + (isPrime(9) ? "is a prime number" : "is NOT a prime number"));
-        System.out.println(10 + " " + (isPrime(10) ? "is a prime number" : "is NOT a prime number"));
+    public static void sumOf3and5(){
+        int count = 0, result = 0;
+        for(int i=1; i <= 1000 && count<5; i++){
+            if(i%5==0 && i%3==0){
+                System.out.println(i + " is divisible both by 3 and 5");
+                count++;
+                result += i;
+            }
+        }
+        System.out.println(result + " is the sum of those numbers divisible both by 3 and 5");
+    }
+
+    static void main(String[] args) {
+        sumOf3and5();
+    }
+
+}
+
+/*public static void  main(String[] args) {
+
+            /*    int primeNumberCounter = 0;
+                for(int counter = 0; counter < 1000; counter++){
+                    if(isPrime(counter)){
+                        ++primeNumberCounter;
+                        System.out.println(counter + " " + (isPrime(counter) ? "is a prime number" : "is NOT a prime number"));
+                        if(primeNumberCounter == 3){
+                            break;
+                        }
+                    }
+                }
+            }
 
     }
+
 }
+            */
