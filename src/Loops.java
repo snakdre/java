a8ed7cc5c7dda3ebc156f114e435ebd6be02ff92
Tonye Andre Martial <tandremartial@internal.salesforce.com>
2026-02-20 19:05:23 -0500

    Adding the Prime function

--- src/Loops.java
+++ src/Loops.java
@@ -9,11 +9,28 @@ public class Loops {
         }
     }
     public static boolean isPrime(int wholeNumber){
-
+        if(wholeNumber <= 2){
+            return (wholeNumber==2);
+        }
+        for(int divisor=2; divisor < wholeNumber; divisor++){
+            if(wholeNumber % divisor == 0){
+                return false;
+            }
+        }
         return true;
     }
 
-    static void main() {
-        interestCalculation(1000);
+    public static void  main(String[] args) {
+        System.out.println(1 + " " + (isPrime(1) ? "is a prime number" : "is NOT a prime number"));
+        System.out.println(2 + " " + (isPrime(2) ? "is a prime number" : "is NOT a prime number"));
+        System.out.println(3 + " " + (isPrime(3) ? "is a prime number" : "is NOT a prime number"));
+        System.out.println(4 + " " + (isPrime(4) ? "is a prime number" : "is NOT a prime number"));
+        System.out.println(5 + " " + (isPrime(5) ? "is a prime number" : "is NOT a prime number"));
+        System.out.println(6 + " " + (isPrime(6) ? "is a prime number" : "is NOT a prime number"));
+        System.out.println(7 + " " + (isPrime(7) ? "is a prime number" : "is NOT a prime number"));
+        System.out.println(8 + " " + (isPrime(8) ? "is a prime number" : "is NOT a prime number"));
+        System.out.println(9 + " " + (isPrime(9) ? "is a prime number" : "is NOT a prime number"));
+        System.out.println(10 + " " + (isPrime(10) ? "is a prime number" : "is NOT a prime number"));
+
     }
 }
