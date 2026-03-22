f2f0011aebcbb2f54e085d12eea8166c9352a639
tandremartial <tandremartial@salesforce.com>
2026-03-21 22:52:14 -0400

    Update groceries list interactions

--- src/Lists/Groceries.java
+++ src/Lists/Groceries.java
@@ -1,32 +1,39 @@
 package Lists;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Groceries {
     public static void menu(){
         System.out.println("AVAILABLE ACTIONS:\n");
         System.out.println("0 - To shutdown\n");
-        System.out.println("1 - To add item(s) in the list (Comma delimited list)");
+        System.out.println("1 - To add item(s) in the list (Comma delimited list)\n");
         System.out.println("2 - To remove any item(s) from the list (Comma delimited list)\n");
         System.out.println("Enter a number for which action you wanna take: ");
     }
     public static void addItem(String[] item, ArrayList<String> lists){
-        StringBuilder missing = new StringBuilder();
+        StringBuilder duplicate = new StringBuilder();
         StringBuilder found = new StringBuilder();
         for(String el : item){
-            if(lists.contains(el)){
+            if(!lists.contains(el)){
                 lists.add(el);
                 found.append(el).append(" ");
             }
             else{ //fixing grammar
-                missing.append(el).append(" ");
-                if (missing.indexOf(" ") >= 0){System.out.println(missing + " are not in the groceries list! ");}
-                else{System.out.println(missing + " is not in the groceries list");}
+                duplicate.append(el).append(" ");
             }
         }
-        System.out.println(found.toString() + " have been added!");
-        System.out.println("Updated groceries list:\n" + lists);
+        if(!found.isEmpty()){
+            System.out.println("Groceries have been added!");
+            lists.sort(Comparator.naturalOrder());
+            System.out.println(lists);
+
+        }
+        if(!duplicate.isEmpty()){
+            System.out.println(duplicate.toString() + "found already in the groceries list! ");
+            System.out.println("No update...:\n");
+        }
     }
     public static void removeItem(String[] item, ArrayList<String> lists){
         StringBuilder missing = new StringBuilder();
@@ -38,18 +45,37 @@ public class Groceries {
             }
             else{ //fixing grammar
                 missing.append(el).append(" ");
-                if (missing.indexOf(" ") >= 0){System.out.println(missing + " are not in the groceries list! ");}
-                else{System.out.println(missing + " is not in the groceries list");}
             }
         }
-        System.out.println(found.toString() + " have been removed!");
-        System.out.println("Updated groceries list:\n" + lists);
+        if(!found.isEmpty()) {System.out.println("Groceries have been removed!");}
+        if(!missing.isEmpty()){System.out.println(missing.toString() + "not found in the groceries list! ");}
+        lists.sort(Comparator.naturalOrder());
     }
 
 
     static void main() {
         ArrayList<String> groceries = new ArrayList<>();
         Scanner input = new Scanner(System.in);
-        menu();
+        String choice = "";
+        boolean quit = true;
+        do{
+            menu();
+            choice = input.nextLine();
+            switch (choice){
+                case "0":
+                    quit = false;
+                    break;
+                case  "1":
+                    System.out.println("Enter the groceries list: ");
+                    String[] inputs = input.nextLine().split(",");
+                    addItem(inputs, groceries);
+                    break;
+                case "2":
+                    System.out.println("Enter the groceries list to remove: ");
+                    String[] inputs2 = input.nextLine().split(",");
+                    removeItem(inputs2, groceries);
+                    break;
+            }
+        }while(quit);
     }
 }
